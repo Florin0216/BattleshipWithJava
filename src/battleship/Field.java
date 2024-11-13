0e247459bdf9f04f8b3c510445dc2a434328bfae
Florin0216 <florin.barbu16@yahoo.com>
2024-11-14 01:17:36 +0200

    second commit

--- src/battleship/Field.java
+++ src/battleship/Field.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
+package battleship;
 
-public class Main {
-
+public class Field {
     public static void displayField(String[][] field){
 
         for(int i = 0; i < field.length; i++ ){
@@ -94,35 +93,6 @@ public class Main {
                     System.out.print(parts[i] + " ");
                 }
             }
-        }
-
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        String[][] field = new String[11][11];
-        displayField(field);
-
-        System.out.println("Enter the coordinates of the ship:");
-        String firstCoordinate = scanner.next();
-        String secondCoordinate = scanner.next();
-
-        int firstIndex = Integer.parseInt(firstCoordinate.substring(1));;
-        int secondIndex= Integer.parseInt(secondCoordinate.substring(1));
-
-        if((firstCoordinate.charAt(0) != secondCoordinate.charAt(0)) && (!firstCoordinate.substring(1).equals(secondCoordinate.substring(1)))) {
-            System.out.println("Error");
-        }else if((firstCoordinate.charAt(0) <'A' || firstCoordinate.charAt(0) >'J')){
-            System.out.println("Error");
-        }else if((secondCoordinate.charAt(0) <'A' || secondCoordinate.charAt(0) >'J')){
-            System.out.println("Error");
-        }else if((firstIndex < 1 || firstIndex > 10)){
-            System.out.println("Error");
-        } else if ((secondIndex < 1 || secondIndex > 10)) {
-            System.out.println("Error");
-        }else {
-            getLengthAndParts(firstCoordinate, secondCoordinate);
         }
     }
 }
---
+++ src/battleship/Main.java
@@ -0,0 +1,34 @@
+package battleship;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[][] field = new String[11][11];
+        Field.displayField(field);;
+
+        System.out.println("Enter the coordinates of the ship:");
+        String firstCoordinate = scanner.next();
+        String secondCoordinate = scanner.next();
+
+        int firstIndex = Integer.parseInt(firstCoordinate.substring(1));;
+        int secondIndex= Integer.parseInt(secondCoordinate.substring(1));
+
+        if((firstCoordinate.charAt(0) != secondCoordinate.charAt(0)) && (!firstCoordinate.substring(1).equals(secondCoordinate.substring(1)))) {
+            System.out.println("Error");
+        }else if((firstCoordinate.charAt(0) <'A' || firstCoordinate.charAt(0) >'J')){
+            System.out.println("Error");
+        }else if((secondCoordinate.charAt(0) <'A' || secondCoordinate.charAt(0) >'J')){
+            System.out.println("Error");
+        }else if((firstIndex < 1 || firstIndex > 10)){
+            System.out.println("Error");
+        } else if ((secondIndex < 1 || secondIndex > 10)) {
+            System.out.println("Error");
+        }else {
+            Field.getLengthAndParts(firstCoordinate, secondCoordinate);
+        }
+    }
+}
